16a68841acb410e0d698dd9bc7fa85b8fd2012fa
Gilbert <gilbertawak47@gmail.com>
2025-05-15 22:36:44 +0700

    nambahin gambar collision buat arena c dan d

--- src/ArenaC.java
+++ src/ArenaC.java
@@ -3,9 +3,9 @@ import javax.swing.JFrame;
 public class ArenaC extends Arena {
     public ArenaC(JFrame parentFrame) {
         super(
-            "RobberyBob/Assets/mapArenaB.jpg", 
-            "RobberyBob/Assets/collisionArenaB.jpg", 
-            170, 100,  // Posisi awal RobberyBob di ArenaB (ubah sesuai kebutuhan)
+            "RobberyBob/Assets/mapArenaC.jpg", 
+            "RobberyBob/Assets/collisionArenaC.jpg", 
+            450, 500,  // Posisi awal RobberyBob di ArenaB (ubah sesuai kebutuhan)
             parentFrame
         );
     }
--- src/ArenaD.java
+++ src/ArenaD.java
@@ -3,9 +3,9 @@ import javax.swing.JFrame;
 public class ArenaD extends Arena {
     public ArenaD(JFrame parentFrame) {
         super(
-            "RobberyBob/Assets/mapArenaB.jpg", 
-            "RobberyBob/Assets/collisionArenaB.jpg", 
-            170, 100,  // Posisi awal RobberyBob di ArenaB (ubah sesuai kebutuhan)
+            "RobberyBob/Assets/mapArenaD.jpg", 
+            "RobberyBob/Assets/collisionArenaD.jpg", 
+            50, 500,  // Posisi awal RobberyBob di ArenaB (ubah sesuai kebutuhan)
             parentFrame
         );
     }
